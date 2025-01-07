15f096e0654c463fd8897e20c38196da12cb11ba
Blair Crumbly <blaircrumbly@gmail.com>
2025-01-07 13:27:27 -0500

    All tests passed, used math floor, random, mult, and  mod

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 
-// Write your code here
-
+//TODO Write your code here
+//! this is improtant and red
+// green
 let num1 = 31
 let num2 = 2
 
 let multiply = num1*num2
+            //rounds number so no float  //celing //makes it between 1 - 1000
+let random = Math.floor(Math.random()* 1000 ) + 1;
+
+let num3 = 16
+let num4 = 6
+
+const mod = num3 % num4;
+const max = Math.max (5, 10, 15, 20);
